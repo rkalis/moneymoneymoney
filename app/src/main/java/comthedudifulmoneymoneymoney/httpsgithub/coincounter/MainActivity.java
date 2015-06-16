36acf02c6a89e5b9dc6cdebf5ff3a8d4f87be16a
Julian Kramer <j.m.r.kramer@gmail.com>
2015-06-16 12:17:23 +0200

    nieuw 2

--- app/src/main/java/comthedudifulmoneymoneymoney/httpsgithub/coincounter/MainActivity.java
+++ app/src/main/java/comthedudifulmoneymoneymoney/httpsgithub/coincounter/MainActivity.java
@@ -32,6 +32,9 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+
+
+
         return super.onOptionsItemSelected(item);
     }
 }
